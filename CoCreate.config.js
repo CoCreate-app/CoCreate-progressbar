6c3ea7d727bbc01ca716e8bab15a729e02de6985
Frank Pagan <frank@cocreate.app>
2021-02-14 21:31:17 +0000

    clean tree

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -9,7 +9,7 @@ module.exports = {
     sources: [{
             path: "./docs/index.html",
             collection: "files",
-            document_id: "",
+            document_id: "602995aae979fb17407e25c5",
             key: "html",
             data:{
                 name: "Progressbar Doc",
@@ -19,10 +19,10 @@ module.exports = {
    
     crud: [{
             collection: "routes",
-            document_id: "",
+            document_id: "602995aae979fb17407e25c4",
             data:{
                 collection: "files",
-                document_id: "",
+                document_id: "602995aae979fb17407e25c5",
                 name: "html",
                 domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
                 route: "/docs/progressbar",
